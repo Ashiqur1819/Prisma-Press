0658cb7c21269007abaee6065e59da2a18189a63
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2026-06-24 09:10:34 +0600

    Create utils funcitons for catch async and send response

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -26,15 +26,20 @@ const registerUserIntoDB = async (payload: IUserPayload) => {
       name,
       email,
       password: hashedPasword,
+      profile: {
+        create: {
+          profilePhoto,
+        },
+      },
     },
   });
 
-  await prisma.profile.create({
-    data: {
-      userId: createdUser.id,
-      profilePhoto,
-    },
-  });
+  // await prisma.profile.create({
+  //   data: {
+  //     userId: createdUser.id,
+  //     profilePhoto,
+  //   },
+  // });
 
   const user = await prisma.user.findUnique({
     where: {
