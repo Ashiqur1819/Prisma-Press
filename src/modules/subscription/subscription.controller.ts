d0d6db72cb0f0b2d937ce98d69dba4b5368b299b
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2026-07-02 09:35:40 +0600

    Install Stripe CLI In Local Machine and format code

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -2,23 +2,25 @@ import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { subscriptionService } from "./subscription.service";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus from "http-status"
+import httpStatus from "http-status";
 
 const createCheckoutSession = catchAsync(
-    async(req: Request, res: Response, next: NextFunction) => {
-        const userId = req.user?.userId
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.userId;
 
-        const result = await subscriptionService.createCheckoutSession(userId as string)
+    const result = await subscriptionService.createCheckoutSession(
+      userId as string,
+    );
 
-        sendResponse(res, {
-            success: true,
-            statusCode: httpStatus.OK,
-            message: "Payment completed sucessfully",
-            data: result
-        })
-    }
-)
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Payment completed sucessfully",
+      data: result,
+    });
+  },
+);
 
 export const subscriptionController = {
-    createCheckoutSession
-}
+  createCheckoutSession,
+};
